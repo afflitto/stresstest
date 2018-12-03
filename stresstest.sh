8844dfb4a3f33c8f4aea687c6b2cba7bc4981d85
afflitto <andrew.afflitto@gmail.com>
2018-12-02 22:03:01 -0500

    add stress test shell script

---
+++ stresstest.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+i=1
+
+while true; do	
+	#calculate length of sysbench test
+	maxprime=$(shuf -i 5000-20000 -n 1)
+	echo "Starting round $i of testing (max prime of $maxprime)"
+	
+	#print current date and time
+	date
+	
+	#print temperature before test
+	echo -n "Temperature (pre test):"
+	vcgencmd measure_temp
+
+	#run test
+	sysbench --num-threads=4 --validate=on --test=cpu --cpu-max-prime=$maxprime run
+	
+	#time in seconds to sleep before next round of testing (15s-2m)
+	sleeptime=$(shuf -i 15-120 -n 1)
+	
+	#print temperature after test
+	echo -n "Temperature (post test):"
+	vcgencmd measure_temp
+	
+	#sleep
+	echo "Sleeping for $sleeptime seconds"
+	sleep $sleeptime
+
+	#increment round
+	i=$((i + 1))
+done
+
